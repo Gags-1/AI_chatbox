8289e5615f58753cacb64f29ec618a981a5c161d
Grenish rai <ariestheracer@gmail.com>
2023-03-03 16:09:19 +0530

    update

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+    <section>
+        <typography>
+            Hello World!
+        </typography>
+    </section>
+};
+
+export default Navbar;
